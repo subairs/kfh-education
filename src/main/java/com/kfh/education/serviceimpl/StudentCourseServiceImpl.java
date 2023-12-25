ac58ba14174afd918c84b5ea006041f9cbcbd197
subairs <subairskpm@gmail.com>
2023-12-25 20:21:05 +0530

    Updated StudentCourseService

--- src/main/java/com/kfh/education/serviceimpl/StudentCourseServiceImpl.java
+++ src/main/java/com/kfh/education/serviceimpl/StudentCourseServiceImpl.java
@@ -23,43 +23,44 @@ import com.kfh.education.service.StudentCourseService;
 
 @Service
 public class StudentCourseServiceImpl implements StudentCourseService {
-	
 
-    private final StudentRepository studentRepository;
-    private final CourseRepository courseRepository;
+	private final StudentRepository studentRepository;
+	private final CourseRepository courseRepository;
 	private final ModelMapper modelMapper;
 
-    @Autowired
-    public StudentCourseServiceImpl(StudentRepository studentRepository,
-    		CourseRepository courseRepository, ModelMapper modelMapper) {
-        this.studentRepository = studentRepository;
-        this.courseRepository = courseRepository;
-        this.modelMapper=modelMapper;
-    }
+	@Autowired
+	public StudentCourseServiceImpl(StudentRepository studentRepository, CourseRepository courseRepository,
+			ModelMapper modelMapper) {
+		this.studentRepository = studentRepository;
+		this.courseRepository = courseRepository;
+		this.modelMapper = modelMapper;
+	}
 
 	@Override
 	public StudentCourseResponse alocateCourseForStudent(long studentId, long courseId) {
-		 	Student student = studentRepository.findById(studentId).orElse(null);
-	        Course course = courseRepository.findById(courseId).orElse(null);
-	        student.setCourse(course);
-	        //studentRepository.save(student);
-	        
-			Student createdStudent;
-			try {
+		Student student = studentRepository.findById(studentId).orElse(null);
+		Course course = courseRepository.findById(courseId).orElse(null);
+		student.setCourse(course);
+		// studentRepository.save(student);
 
+		Student createdStudent = null;
+		try {
+			if (student != null && course != null) {
 				// Save the Course entity using Student repository
 				createdStudent = studentRepository.save(student);
-				
-			} catch (InternalServerError ex) {
-				throw new ServerSideException(ex.getMessage());
-			} catch (Exception ex) {
-				// Catch any other unexpected exceptions
-				throw new CustomException("An error occurred during student save: " + ex.getMessage());
+
 			}
-	        
-	    	// Map the existing Student to Student dto
-			return modelMapper.map(createdStudent, StudentCourseResponse.class);
-	
+
+		} catch (InternalServerError ex) {
+			throw new ServerSideException(ex.getMessage());
+		} catch (Exception ex) {
+			// Catch any other unexpected exceptions
+			throw new CustomException("An error occurred during student save: " + ex.getMessage());
+		}
+
+		// Map the existing Student to Student dto
+		return modelMapper.map(createdStudent, StudentCourseResponse.class);
+
 	}
 
 }
